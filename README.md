34cdd73ec3f0f2c5016a47c091d555b792ca3880
Silvano Seva <silseva@fastwebnet.it>
2020-03-28 10:40:47 +0100

    Second test, debugging in progress.

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # MD380-boot-tests
 Here we're trying to create custom firmware images for Tytera MD380, to be flashed and booted using stock recovery bootloader.  
 Building tests requires arm-none-eabi-gcc.
+  
+  
 
 ### Test 1:
 Simple baremetal image blinking simultaneously both the LEDs. Interrupts disabled, .data and .bss are empty.  
+  
 **Status**: BOOTS OK.
+  
+  
+### Test 2:
+Baremetal image with green LED fixed and red LED bliking.
+Interrupts enabled, SCB_VTOR = 0x0800C000, .data and .bss initialised and libc integration.
+  
+**Status**: TESTING.
---
+++ test_2/main.cpp
@@ -0,0 +1,78 @@
+/***************************************************************************
+ *   Copyright (C) 2020 by Silvano Seva                                    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   As a special exception, if other files instantiate templates or use   *
+ *   macros or inline functions from this file, or you compile this file   *
+ *   and link it with other works to produce a work based on this file,    *
+ *   this file does not by itself cause the resulting work to be covered   *
+ *   by the GNU General Public License. However the source code for this   *
+ *   file must still be made available in accordance with the GNU General  *
+ *   Public License. This exception does not invalidate any other reasons  *
+ *   why a work based on this file might be covered by the GNU General     *
+ *   Public License.                                                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
+ ***************************************************************************/
+
+#include "stm32f405xx.h"
+
+template <unsigned int P, unsigned char N>
+class Pin
+{
+public:
+    static void high()
+    {
+        reinterpret_cast<GPIO_TypeDef*>(P)->BSRRL |= N;
+    }
+
+    static void low()
+    {
+        reinterpret_cast<GPIO_TypeDef*>(P)->BSRRH |= N;
+    }
+
+private:
+    Pin();
+};
+
+static void delay()
+{
+    volatile int i;
+    int j=0;
+    for(i=0;i<1000000;i++)
+    {
+        j++;
+    }
+}
+
+using red = Pin<GPIOE_BASE, 0>;
+using grn = Pin<GPIOE_BASE, 1>;
+
+int main()
+{
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOEEN;
+    GPIOE->MODER |= (1 << 2) | 1;       // PE0 and PE1 outputs
+
+    grn::high();
+    red::low();
+
+    while(1)
+    {
+        red::high();
+        delay();
+        red::low();
+        delay();
+    }
+
+    return 0;
+}
